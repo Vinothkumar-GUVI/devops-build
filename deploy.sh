232fa082d050fded10f52becfca4fe40e73dc264
Vinothkumar-GUVI <kumarvinoth1553@gmail.com>
2024-10-25 18:12:59 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-# Deploy Docker image to the server
-
-# Pull the latest image from Docker Hub
-IMAGE_NAME="vinothkumar017/react-app"
-docker pull $IMAGE_NAME:dev
-
-# Run the container on port 80
-docker run -d -p 80:80 $IMAGE_NAME:dev
-
+# Start the application
+docker-compose up -d
