96e4cb2cf439eaf6b7171e8bd87789087d756732
Vinothkumar-GUVI <kumarvinoth1553@gmail.com>
2024-10-28 17:53:10 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ IMAGE_NAME="vinothkumar017/react-app"
 
 # Build the Docker image
 docker build -t $IMAGE_NAME:dev .
-
+docker login -u vinothkumar017 -p dckr_pat_mCO-f5YBTa26fTdTmfKbLdlzn1g
 # Push the image to Docker Hub (dev repo)
-# docker push $IMAGE_NAME:dev
+docker push $IMAGE_NAME:dev
