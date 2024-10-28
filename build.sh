e6ccf9e3d04e9600beb19e129c9fb72430e83fc5
Vinothkumar-GUVI <kumarvinoth1553@gmail.com>
2024-10-28 17:34:06 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+# Build Docker image for the application
+
+# Specify the image name
+IMAGE_NAME="vinothkumar017/react-app"
+
 # Build the Docker image
-docker-compose build
+docker build -t $IMAGE_NAME:dev .
+
+# Push the image to Docker Hub (dev repo)
+# docker push $IMAGE_NAME:dev
